fa689b5f38e4a80b18cd95b199d0b32306beafc3
Lucien George <lucien.george@shopify.com>
2022-11-11 18:36:51 +0000

    wip: shuffle dice on button click event

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // linking UI elements to ViewController
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    @IBAction func onRollClick(_ sender: UIButton) {
+        let images: [String] = [
+            "DiceOne",
+            "DiceTwo",
+            "DiceThree",
+            "DiceFour",
+            "DiceFive",
+            "DiceSix"
+        ]
+        
+        let imageOne = UIImage(named: images.randomElement()!)
+        let imageTwo = UIImage(named: images.randomElement()!)
+        diceImageViewOne.image = imageOne
+        diceImageViewTwo.image = imageTwo
+    }
 }
-
